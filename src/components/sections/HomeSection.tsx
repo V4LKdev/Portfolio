ed203ac0167dc0ec28c5f1e04fd18ba237e0766d
Nicolas Martin <nico@martin.li>
2025-06-24 20:09:01 +0200

    step 8: update HomeSection to use HomeSectionComponent generic type

--- src/components/sections/HomeSection.tsx
+++ src/components/sections/HomeSection.tsx
@@ -3,13 +3,13 @@
 // Displays personal information and main navigation button
 
 import React from "react";
-import { HomeSectionProps } from "../../types/SharedProps";
+import { HomeSectionComponent } from "../../types/SharedProps";
 
 /**
  * Home section component - main landing page content
  * @param onNavigateToProjects - Callback to navigate to projects section (unused in current design)
  */
-const HomeSection: React.FC<HomeSectionProps> = ({ onNavigateToProjects: _onNavigateToProjects, className, id }) => {
+const HomeSection: HomeSectionComponent = ({ onNavigateToProjects: _onNavigateToProjects, className, id }) => {
   return (
     <div className={`text-left max-w-4xl pt-8 transition-all duration-500 animate-fade-in no-select ${className || ""}`} id={id}>
       {/* Content removed for clean video game menu aesthetic */}
